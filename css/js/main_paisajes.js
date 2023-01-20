78a46a55e3552fa3aa903dce8cd2339c04c42dbe
agomcam <agomcam813@iescarrillo.es>
2023-01-20 20:54:13 +0100

    al pasar el raton por lo alto de las fotos de paisaje se ponen de un color mas claro

---
+++ css/js/main_paisajes.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    $(".foto1").mouseover(function(){
+        $(".foto1").css("opacity", "1");
+    });
+    $(".foto1").mouseout(function(){
+        $(".foto1").css("opacity", "0.5");
+    });
+});
+
+// 
+$(document).ready(function(){
+    $(".foto2").mouseover(function(){
+        $(".foto2").css("opacity", "1");
+    });
+    $(".foto2").mouseout(function(){
+        $(".foto2").css("opacity", "0.5");
+    });
+});
+// 
+$(document).ready(function(){
+    $(".foto3").mouseover(function(){
+        $(".foto3").css("opacity", "1");
+    });
+    $(".foto3").mouseout(function(){
+        $(".foto3").css("opacity", "0.5");
+    });
+});
+// 
+$(document).ready(function(){
+    $(".foto4").mouseover(function(){
+        $(".foto4").css("opacity", "1");
+    });
+    $(".foto4").mouseout(function(){
+        $(".foto4").css("opacity", "0.5");
+    });
+});
+// 
+$(document).ready(function(){
+    $(".foto5").mouseover(function(){
+        $(".foto5").css("opacity", "1");
+    });
+    $(".foto5").mouseout(function(){
+        $(".foto5").css("opacity", "0.5");
+    });
+});
+// 
+$(document).ready(function(){
+    $(".foto6").mouseover(function(){
+        $(".foto6").css("opacity", "1");
+    });
+    $(".foto6").mouseout(function(){
+        $(".foto6").css("opacity", "0.5");
+    });
+});
